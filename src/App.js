6e67fbefc4b26c66b99925ccb59c16d2e2eb20d4
Sarn Sornpaisarn <fsornpaisarn@gmail.com>
2019-02-03 18:18:36 +0700

    push test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="text-center pt-5">Hello World</h1>
+        <h1 className="text-center pt-5">React Version</h1>
+        <div className="text-center display-2">{React.version}</div>
       </div>
     );
   }
