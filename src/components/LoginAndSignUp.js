fed7a423144178f33c5249d1050c26658918d88f
Sarn Sornpaisarn <fsornpaisarn@gmail.com>
2019-02-04 22:45:21 +0700

    fixed login

--- src/components/LoginAndSignUp.js
+++ src/components/LoginAndSignUp.js
@@ -1,60 +1,59 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 export default props => {
   return (
     <div>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav ml-auto">
-          <li className="nav-item mr-3">
-            <div className="container">
-              <button
-                className="btn btn-rounded btn-light-orange"
-                data-toggle="modal"
-                data-target="#loginModal"
-              >
-                LOGIN
-              </button>
-              <div className="modal" id="loginModal">
-                <div className="modal-dialog">
-                  <div className="modal-content">
-                    <div className="modal-header">
-                      <h5 className="modal-title">Login</h5>
-                      <button className="close" data-dismiss="modal">
-                        &times;
-                      </button>
-                    </div>
-                    <div className="modal-body">
-                      <form>
-                        <div className="form-group">
-                          <label for="username">Username</label>
-                          <input
-                            type="text"
-                            placeholder="Username"
-                            className="form-control"
-                          />
-                        </div>
-                        <div className="form-group">
-                          <label for="password">Password</label>
-                          <input
-                            type="password"
-                            placeholder="Password"
-                            className="form-control"
-                          />
-                        </div>
-                      </form>
-                    </div>
-                    <div className="modal-footer">
-                      <button className="btn btn-primary" data-dismiss="modal">
-                        Login
-                      </button>
-                    </div>
+          <li className="nav-item mr-4">
+            <a
+              className="nav-link"
+              href="#"
+              data-toggle="modal"
+              data-target="#loginModal"
+            >
+              LOGIN
+            </a>
+            <div className="modal" id="loginModal">
+              <div className="modal-dialog">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h5 className="modal-title">Login</h5>
+                    <button className="close" data-dismiss="modal">
+                      &times;
+                    </button>
+                  </div>
+                  <div className="modal-body">
+                    <form>
+                      <div className="form-group">
+                        <label for="username">Username</label>
+                        <input
+                          type="text"
+                          placeholder="Username"
+                          className="form-control"
+                        />
+                      </div>
+                      <div className="form-group">
+                        <label for="password">Password</label>
+                        <input
+                          type="password"
+                          placeholder="Password"
+                          className="form-control"
+                        />
+                      </div>
+                    </form>
+                  </div>
+                  <div className="modal-footer">
+                    <button className="btn btn-primary" data-dismiss="modal">
+                      Login
+                    </button>
                   </div>
                 </div>
               </div>
             </div>
           </li>
           <li>
-            <button className="btn btn-orange btn-rounded " data-toggle="modal">
+            <button className="btn btn-orange btn-rounded" data-toggle="modal">
               SIGN UP
             </button>
           </li>
