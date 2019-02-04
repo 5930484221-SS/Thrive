788848cace4c1b388abea2086f64e207d32f361b
Thamonwan Tangchitdee <Thamonwan_most@hotmail.com>
2019-02-04 11:55:04 +0700

    change button color

--- src/components/LandingText.js
+++ src/components/LandingText.js
@@ -5,30 +5,30 @@ export default props => {
   return (
     <div>
       <section id="showcase">
-        <div class="container new-section">
-          <div class="row">
-            <div class="col-md-2" />
-            <div class="col-lg-6 col-md-8">
-              <div class="showcase-header">
+        <div className="container new-section">
+          <div className="row">
+            <div className="col-md-2" />
+            <div className="col-lg-6 col-md-8">
+              <div className="showcase-header">
                 The{" "}
-                <span class="text-orange">
+                <span className="text-orange">
                   <strong>BEST</strong>
                 </span>{" "}
                 Tutor & Learner Matching System
               </div>
-              <p class="text-muted mt-3 showcase-text">
+              <p className="text-muted mt-3 showcase-text">
                 Find your favorite tutor and study with your own pace wherever
                 you want
               </p>
               <div id="big-button">
-                <button class="btn btn-secondary btn-rounded btn-extra mt-4">
+                <button className="btn btn-primary btn-rounded btn-extra mt-4">
                   SIGN UP
                 </button>
               </div>
             </div>
-            <div class="col-md-2">
+            <div className="col-md-2">
               <img
-                class="img-fluid rocket-with-shadow d-none d-lg-block about-img"
+                className="img-fluid rocket-with-shadow d-none d-lg-block about-img"
                 src={rocketWithShadow}
                 alt=""
               />
