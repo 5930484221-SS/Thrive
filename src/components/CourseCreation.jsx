512d39dadc86a869036e6065773414327947c2e3
5930486521-ST <sirawit_james@hotmail.co.th>
2019-02-05 00:43:22 +0700

    add state and some handlers

--- src/components/CourseCreation.jsx
+++ src/components/CourseCreation.jsx
@@ -1,33 +1,78 @@
-import axios from 'axios';
-import querystring from 'query-string';
-import React, { Component } from 'react';
-import CowBg from './CowBg';
+import axios from "axios";
+import querystring from "query-string";
+import React, { Component } from "react";
+import CowBg from "./CowBg";
+import { Courses } from "./Courses";
+import Loader from "./Loader";
 
-import './courseCreate.css';
+import "./courseCreate.css";
 
 class CourseCreation extends Component {
   course_name = React.createRef();
-  state = {};
+
+  constructor() {
+    super();
+    this.state = {
+      topic: "",
+      description: "",
+      descriptionProfile: "",
+      subject: "",
+      duration: "",
+      location: "",
+      tuition: "",
+      fee: "",
+      isLoading: false
+    };
+  }
+
+  handleInputChange = event => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    if (name === "fee" || name === "tuition") {
+      if (isNaN(value) || value.includes("-")) return;
+    }
+    this.setState({
+      [name]: value
+    });
+  };
 
   onSubmit = async e => {
+    this.setState({ isLoading: true });
     e.preventDefault();
-    axios({
-      method: 'POST',
-      url: 'http://localhost:8000/api/create_course',
-      crossDomain: true,
-      data: querystring.stringify({
-        token: localStorage.getItem('token'),
-        course_name: this.course_name.current.value
-      }),
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      }
-    })
-      .then(response => alert('ok'))
-      .catch(error => alert('failed'));
+    try {
+      const response = await axios({
+        method: "POST",
+        url: "http://localhost:8000/api/create_course",
+        crossDomain: true,
+        data: querystring.stringify({
+          token: localStorage.getItem("token"),
+          course_name: this.state.topic
+        }),
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        }
+      });
+      alert("ok");
+    } catch (error) {
+      alert("failed");
+    }
+    this.setState({ isLoading: false });
   };
 
   render() {
+    console.log(this.state);
+    const {
+      topic,
+      description,
+      descriptionProfile,
+      subject,
+      duration,
+      location,
+      tuition,
+      fee,
+      isLoading
+    } = this.state;
     return (
       <div>
         <CowBg />
@@ -39,7 +84,7 @@ class CourseCreation extends Component {
                   type="button"
                   className="btn btn-outline-dark uploadPic"
                 >
-                  Upload Image
+                  <input type="file" onChange={this.handleInputChange} />
                 </button>
               </div>
               <div className="form-group col-md-11">
@@ -49,8 +94,12 @@ class CourseCreation extends Component {
                 <textarea
                   className="form-control"
                   id="descriptionProfile"
+                  name="descriptionProfile"
                   rows="15"
                   placeholder="Describe your profile"
+                  onChange={this.handleInputChange}
+                  value={descriptionProfile}
+                  disabled = {isLoading}
                 />
               </div>
             </div>
@@ -63,65 +112,73 @@ class CourseCreation extends Component {
                   type="text"
                   className="form-control"
                   id="topic"
+                  name="topic"
                   placeholder="Enter topic here"
+                  onChange={this.handleInputChange}
+                  value={topic}
+                  disabled = {isLoading}
                 />
               </div>
+
               <br />
 
               <span className="topic">
-                {' '}
+                {" "}
                 <span className="text-orange text-extra">A</span>
-                bout <span className="text-orange text-slim">Course</span>{' '}
+                bout <span className="text-orange text-slim">Course</span>{" "}
               </span>
               <div className="form-group">
                 <label htmlFor="subject">Subject</label>
-                <select className="form-control" id="subject">
-                  <option>Mathematics</option>
-                  <option>Science</option>
-                  <option>Physics</option>
-                  <option>Chemical</option>
-                  <option>Biology</option>
-                  <option>Astronomy</option>
-                  <option>Basic Science</option>
-                  <option>Thai</option>
-                  <option>Social</option>
-                  <option>GAT-Thai</option>
-                  <option>GAT-Eng</option>
-                  <option>English</option>
-                  <option>France</option>
-                  <option>China</option>
-                  <option>Japanese</option>
-                  <option>Korea</option>
-                  <option>Pat1</option>
-                  <option>Pat2</option>
-                  <option>Pat3</option>
-                  <option>CU-TEP</option>
-                  <option>TU GET</option>
+                <select
+                  className="form-control"
+                  id="subject"
+                  name="subject"
+                  onChange={this.handleInputChange}
+                  value={subject}
+                  disabled = {isLoading}
+                >
+                  {Courses.map(course => (
+                    <option key={course}>{course}</option>
+                  ))}
                 </select>
+
                 <label htmlFor="description">Description</label>
                 <textarea
                   className="form-control"
                   id="description"
+                  name="description"
                   rows="3"
                   placeholder="Describe your course"
+                  onChange={this.handleInputChange}
+                  value={description}
+                  disabled = {isLoading}
                 />
 
                 <label htmlFor="location">Location</label>
                 <textarea
                   className="form-control"
                   id="location"
+                  name="location"
                   rows="3"
                   placeholder="ex: Sukhumvit Line, Siam"
+                  onChange={this.handleInputChange}
+                  value={location}
+                  disabled = {isLoading}
                 />
 
                 <label htmlFor="duration">Duration</label>
                 <textarea
                   className="form-control"
                   id="duration"
+                  name="duration"
                   rows="3"
                   placeholder="Date and Time"
+                  onChange={this.handleInputChange}
+                  value={duration}
+                  disabled = {isLoading}
                 />
               </div>
+
               <br />
 
               <span className="topic"> Charge </span>
@@ -129,19 +186,27 @@ class CourseCreation extends Component {
                 <div className="form-group col-md-6">
                   <label htmlFor="tuition">Tuition/Hour</label>
                   <input
-                    type="number"
+                    type="text"
                     className="form-control"
                     id="tuition"
+                    name="tuition"
                     step="10"
+                    onChange={this.handleInputChange}
+                    value={tuition}
+                    disabled = {isLoading}
                   />
                 </div>
                 <div className="form-group col-md-6">
                   <label htmlFor="fee">Joining fee</label>
                   <input
-                    type="number"
+                    type="text"
                     className="form-control"
                     id="fee"
+                    name="fee"
                     step="10"
+                    onChange={this.handleInputChange}
+                    value={fee}
+                    disabled = {isLoading}
                   />
                 </div>
               </div>
@@ -154,10 +219,9 @@ class CourseCreation extends Component {
             />
 
             <br />
-            <input type="text" ref={this.course_name} />
-            <input type="submit" value="Submit" />
           </div>
         </form>
+        {isLoading? Loader:null}
       </div>
     );
   }
---
+++ src/components/Loader.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default (
+  <div className="lds-css ng-scope" style={{ width: "200px", height: "200px" }}>
+    <div style={{ width: "100%", height: "100%" }} className="lds-ellipsis">
+      <div>
+        <div />
+      </div>
+      <div>
+        <div />
+      </div>
+      <div>
+        <div />
+      </div>
+      <div>
+        <div />
+      </div>
+      <div>
+        <div />
+      </div>
+    </div>
+  </div>
+);
