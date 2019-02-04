341dc9d77f8fc30fd18778742e7c9ae042e4b70c
Thamonwan Tangchitdee <Thamonwan_most@hotmail.com>
2019-02-04 19:37:36 +0700

    add Login modal

--- src/components/LoginAndSignUp.js
+++ src/components/LoginAndSignUp.js
@@ -6,12 +6,55 @@ export default props => {
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav ml-auto">
           <li className="nav-item mr-3">
-            <a className="nav-link" href="#">
-              LOGIN
-            </a>
+            <div className="container">
+              <button
+                className="btn btn-rounded btn-light-orange"
+                data-toggle="modal"
+                data-target="#loginModal"
+              >
+                LOGIN
+              </button>
+              <div className="modal" id="loginModal">
+                <div className="modal-dialog">
+                  <div className="modal-content">
+                    <div className="modal-header">
+                      <h5 className="modal-title">Login</h5>
+                      <button className="close" data-dismiss="modal">
+                        &times;
+                      </button>
+                    </div>
+                    <div className="modal-body">
+                      <form>
+                        <div className="form-group">
+                          <label for="username">Username</label>
+                          <input
+                            type="text"
+                            placeholder="Username"
+                            className="form-control"
+                          />
+                        </div>
+                        <div className="form-group">
+                          <label for="password">Password</label>
+                          <input
+                            type="password"
+                            placeholder="Password"
+                            className="form-control"
+                          />
+                        </div>
+                      </form>
+                    </div>
+                    <div className="modal-footer">
+                      <button className="btn btn-primary" data-dismiss="modal">
+                        Login
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </li>
           <li>
-            <button id="nav-signup" className="btn btn-orange btn-rounded">
+            <button className="btn btn-orange btn-rounded " data-toggle="modal">
               SIGN UP
             </button>
           </li>
