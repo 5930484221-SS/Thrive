4de460a09146db4cbb0e979bbc1e2d42d520256b
Thanapat Juthavantana <thanapat.jutha@gmail.com>
2019-02-05 15:50:20 +0700

    nothing

--- src/components/LoginAndSignUp.js
+++ src/components/LoginAndSignUp.js
@@ -2,64 +2,46 @@ import React, { Component } from 'react';
 
 export default props => {
   return (
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav ml-auto">
-        <li className="nav-item mr-4">
-          <a
-            className="nav-link"
-            href="#"
-            data-toggle="modal"
-            data-target="#loginModal"
-          >
-            LOGIN
-          </a>
-          <div className="modal" id="loginModal">
-            <div className="modal-dialog">
-              <div className="modal-content">
-                <div className="modal-header">
-                  <h5 className="modal-title">Login</h5>
-                  <button className="close" data-dismiss="modal">
-                    &times;
-                  </button>
-                </div>
-                <div className="modal-body">
-                  <form>
-                    <div className="form-group">
-                      <label htmlFor="username">Username</label>
-                      <input
-                        type="text"
-                        placeholder="Username"
-                        className="form-control"
-                      />
-                    </div>
-                    <div className="form-group">
-                      <label htmlFor="password">Password</label>
-                      <input
-                        type="password"
-                        placeholder="Password"
-                        className="form-control"
-                      />
-                    </div>
-                  </form>
-                </div>
-                <div className="modal-footer">
-                  <button
-                    className="btn btn-lg btn-orange"
-                    data-dismiss="modal"
-                  >
-                    Login
-                  </button>
-                </div>
-              </div>
-            </div>
+    <div className="modal" id="loginModal">
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title">Login</h5>
+            <button className="close" data-dismiss="modal">
+              &times;
+            </button>
           </div>
-        </li>
-        <li>
-          <button className="btn btn-orange btn-rounded" data-toggle="modal">
-            SIGN UP
-          </button>
-        </li>
-      </ul>
+          <div className="modal-body">
+            <form onSubmit={props.onLogin}>
+              <div className="form-group">
+                <label htmlFor="username">Username</label>
+                <input
+                  type="text"
+                  placeholder="Username"
+                  className="form-control"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  placeholder="Password"
+                  className="form-control"
+                />
+              </div>
+            </form>
+          </div>
+          <div className="modal-footer">
+            <button
+              className="btn btn-lg btn-orange"
+              data-dismiss="modal"
+              type="submit"
+            >
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,72 @@
 import React, { Component } from 'react';
+import axios from "axios";
+import querystring from "query-string";
 import LoginAndSignUp from './LoginAndSignUp';
 import rocketLogo from '../img/rocket_logo.svg';
 
-export default props => {
-  return (
-    <nav className="navbar navbar-expand-sm navbar-light navbar-light-orange">
-      <div className="container">
-        <a href="#" className="navbar-brand text-secondary">
-          <img src={rocketLogo} />
-          THRIVE.
-        </a>
-        <button
-          className="navbar-toggler"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        <LoginAndSignUp />
-      </div>
-    </nav>
-  );
+class NavBar extends Component {
+
+  onLogin =  async e => {
+    console.log('login')
+    // e.preventDefault();
+    // const response = await axios({
+    //   method: 'POST',
+    //   url: 'http://localhost:8000/api/login',
+    //   crossDomain: true,
+    //   data: querystring.stringify({
+    //     username: this.username.current.value,
+    //     password: this.password.current.value
+    //   }),
+    //   headers: {
+    //     'Content-Type': 'application/x-www-form-urlencoded'
+    //   }
+    // });
+    //
+    // localStorage.setItem('token', response.data.token);
+    // alert('ok');
+    // window.location = '/';
+  };
+
+  render() {
+    return (
+      <nav className="navbar navbar-expand-sm navbar-light navbar-light-orange">
+        <div className="container">
+          <a href="#" className="navbar-brand text-secondary">
+            <img src={rocketLogo} />
+            THRIVE.
+          </a>
+          <button
+            className="navbar-toggler"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item mr-4">
+                <a
+                  className="nav-link"
+                  href="#"
+                  data-toggle="modal"
+                  data-target="#loginModal"
+                >
+                  LOGIN
+                </a>
+              </li>
+              <li>
+                <button className="btn btn-orange btn-rounded" data-toggle="modal">
+                  SIGN UP
+                </button>
+              </li>
+            </ul>
+          </div>
+          <LoginAndSignUp onLogout={this.onLogin} />
+        </div>
+      </nav>
+    );
+  }
 };
+
+export default NavBar;
