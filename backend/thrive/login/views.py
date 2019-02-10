f4b5d7f4e027c6941a52ec60c2f5367f5a3ffbd9
SSripilaipong <SSripilaipong@github.com>
2019-02-10 21:05:16 +0700

    update backend to support searching (courses and tutors)

--- backend/thrive/login/views.py
+++ backend/thrive/login/views.py
@@ -1,4 +1,7 @@
+import re
 import secrets
+from bson.objectid import ObjectId
+from bson.son import SON
 from django.http import (HttpResponseBadRequest, HttpResponseNotFound, JsonResponse,
                          HttpResponseForbidden, HttpResponse)
 from django.views.decorators.csrf import csrf_exempt
@@ -9,6 +12,8 @@ from thrive.mongo_connection import mongo_db
 
 course_fields = ['topic', 'description', 'descriptionProfile', 'duration',
                  'fee', 'location', 'subject', 'tuition', 'img']
+user_detail_fields = ['user', 'display']
+course_number_fields = ['fee', 'tuition', 'rating']
 
 
 def set_response_header(response):
@@ -73,25 +78,149 @@ def create_course(request):
     user = get_username_from_token(token)
     
     if user is None:
-        print(456)
         return HttpResponseForbidden("please login first")
 
-    record = dict()
+    record = dict(tutor=user)
     for field in course_fields:
-        record[field] = request.POST.get(field)
+        value = request.POST.get(field)
+        if field in course_number_fields:
+            value = float(value)
+        record[field] = value
+
+    record['rating_1'] = 0
+    record['rating_2'] = 0
+    record['rating_3'] = 0
+    record['rating_4'] = 0
+    record['rating_5'] = 0
+
     collection = mongo_db.get_collection('courses')
     collection.insert_one(record)
 
     return HttpResponse('')
 
 
+def get_course_query_object(data):
+    fields_exact = ['subject', 'tutor']
+    fields_substring = ['description', 'descriptionProfile', 'location', 'topic']
+    fields_range = ['fee', 'tuition', 'rating']
+
+    query_object = dict()
+
+    for field in fields_exact:
+        value = data.get(field)
+        if value:
+            query_object[field] = re.compile(rf'^{value}$', re.IGNORECASE)
+
+    for field in fields_substring:
+        value = data.get(field)
+        if value:
+            query_object[field] = re.compile(rf'.*{value}.*', re.IGNORECASE)
+
+    for field in fields_range:
+        value_min = data.get(f'{field}Min')
+        value_max = data.get(f'{field}Max')
+
+        q = dict()
+        if value_min:
+            q['$gte'] = float(value_min)
+        if value_max:
+            q['$lte'] = float(value_max)
+
+        if q:
+            query_object[field] = q
+
+    return query_object
+
+
 @csrf_exempt
 @require_http_methods(["POST", "GET"])
 def get_courses(request):
     collection = mongo_db.get_collection('courses')
-    courses = list({s:t[s] for s in course_fields} for t in collection.find({}))
+
+    limit = request.GET.get('limit')
+    last_id = request.GET.get('lastId')
+    _id = request.GET.get('id')
+    order = int(request.GET.get('order', 1))
+
+    qobj = dict()
+    if _id:
+        qobj = {'_id': ObjectId(_id)}
+    else:
+        qobj = get_course_query_object(request.GET)
+        if last_id:
+            cmp = '$gt' if order == 1 else '$lt'
+            qobj['_id'] = {cmp: ObjectId(last_id)}
+
+    lookup_stage = {'as': 'tutor_detail', 'foreignField': 'user', 'from': 'users', 'localField': 'tutor'}
+
+    pipeline = [{'$match': qobj},
+                {'$lookup': lookup_stage},
+                {'$sort': SON([('_id', order)])}]
+    if limit:
+        pipeline.append({'$limit': int(limit)})
+
+    query = collection.aggregate(pipeline)
+
+    courses = []
+    for record in query:
+        course = {field: str(record[field]) for field in course_fields + ['_id']}
+        course['tutor'] = record['tutor']
+        course['tutor_display'] = record['tutor_detail'][0]['display']
+        courses.append(course)
 
     response = JsonResponse(dict(courses=courses))
+
+    return set_response_header(response)
+
+
+@csrf_exempt
+@require_http_methods(["POST", "GET"])
+def get_tutors(request):
+    collection = mongo_db.get_collection('users')
+
+    limit = request.GET.get('limit')
+    last_id = request.GET.get('lastId')
+    _id = request.GET.get('id')
+    order = int(request.GET.get('order', 1))
+
+    user = request.GET.get('user')
+    display = request.GET.get('display')
+
+    qobj = dict()
+    if _id:
+        qobj = {'_id': ObjectId(_id)}
+    elif user:
+        qobj = {'user': user}
+    else:
+        if display:
+            qobj['display'] = re.compile(rf'.*{display}.*', re.I)
+
+        if last_id:
+            cmp = '$gt' if order == 1 else '$lt'
+            qobj['_id'] = {cmp: ObjectId(last_id)}
+
+    lookup_stage = {'from': 'courses',
+                    'let': {'tutor': '$user'},
+                    'pipeline': [{'$match': {'$expr': {'$eq': ['$tutor', '$$tutor']}}},
+                                 {'$count': 'n_courses'}],
+                    'as': 'courses'}
+
+    pipeline = [{'$match': qobj},
+                {'$lookup': lookup_stage},
+                {'$sort': SON([('_id', order)])},
+                {'$match': {'courses': {'$not': {'$size': 0}}}}]
+    if limit:
+        pipeline.append({'$limit': int(limit)})
+
+    query = collection.aggregate(pipeline)
+
+    tutors = []
+    for record in query:
+        tutor = {field: str(record[field]) for field in user_detail_fields + ['_id']}
+        tutor['n_courses'] = record['courses'][0]['n_courses']
+        tutors.append(tutor)
+
+    response = JsonResponse(dict(courses=tutors))
 
     return set_response_header(response)
 
@@ -102,5 +231,30 @@ def logout(request):
     token = request.POST.get("token")
     collection = mongo_db.get_collection('active_token')
     collection.delete_many({'token': token})
+
+    return HttpResponse('')
+
+
+# sprint2
+@csrf_exempt
+@require_http_methods(["POST"])
+def edit_course(request):
+    token = request.POST.get('token')
+    user = get_username_from_token(token)
+
+    if user is None:
+        return HttpResponseForbidden("please login first")
+
+    _id = request.POST.get('id')
+
+    record = dict()
+    for field in course_fields:
+        value = request.POST.get(field)
+        if field in course_number_fields:
+            value = float(value)
+        record[field] = value
+
+    collection = mongo_db.get_collection('courses')
+    collection.update({'_id': _id}, {'$set': record})
 
     return HttpResponse('')
