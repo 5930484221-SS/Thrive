5b0089d3ffae1898ccc74419b33527d8b3d8dcdf
Thanunya Jeerasukvet <b.beemmps@gmail.com>
2019-02-12 21:14:32 +0700

    MyCourse Page

--- src/components/dropDown/MyCourses.js
+++ src/components/dropDown/MyCourses.js
@@ -2,35 +2,56 @@ import React, { Component } from 'react'
 import CowBg from '../CowBg';
 import './myCourses.css';
 import '../courseListing/CourseContainer.css';
-import books from './books.svg';
-import teacher from './teacher.svg';
-import CourseContainer from '../courseListing/CourseContainer';
 import MyCoursesContainer from './MyCoursesContainer';
+import books from '../../img/books.svg';
+import teacher from '../../img/teacher.svg';
 
 export default class MyCourses extends Component {
-   
+  constructor() {
+    super()
+    this.state = {
+      courseByTutor: [],
+    }
+  }
+
+  async componentDidMount() {
+    const res = await fetch("http://127.0.0.1:8000/api/get_courses?tutor=beem")
+    const data = await res.json()
+    // console.log("json",JSON.stringify(data))
+    console.log("obj", data)
+    this.setState({courseByTutor:data.courses})
+
+
+  }
+
   render() {
-  
+    console.log("state",this.state.courseByTutor)
     return (
       <div>
         <CowBg />
-        <div class="topic">
-            <h1 class="display-4">My Courses</h1>
-            <hr />
+        <div className="topic">
+          <h1 className="display-4">My Courses</h1>
+          <hr />
         </div>
-            <span class="display-4 topic" style={{fontSize:"30px"}}> As a Tutor</span> 
-            <img src={teacher} style={{width:"40px",height:"auto"}}/>
-            <br />
-            <MyCoursesContainer />
+        <span className="display-4 topic" style={{ fontSize: "30px" }}> As a Tutor</span>
+        <img src={teacher} style={{ width: "40px", height: "auto" }} />
+        <br />
+        <br />
         
-            <br />
-            <br />
-            <span class="display-4 topic" style={{fontSize:"30px"}}> As a Learner</span>
-            <img src={books} style={{width:"40px",height:"auto"}}/>
-            <MyCoursesContainer />
-       
-
-
+        <div className="row">
+          <div className="card-deck">
+            {this.state.courseByTutor.map((course, index)=>
+              <MyCoursesContainer key={index} info={course} index={index} />
+            )}
+        </div>
+        </div>
+        <br />
+        <br />
+        <span class="display-4 topic" style={{ fontSize: "30px" }}> As a Learner</span>
+        <img src={books} style={{ width: "40px", height: "auto" }} />
+        <br />
+        <br />
+        {/* <MyCoursesContainer /> */}
 
       </div>
     )
--- src/components/dropDown/MyCoursesContainer.js
+++ src/components/dropDown/MyCoursesContainer.js
@@ -1,27 +1,75 @@
 import React, { Component } from 'react'
+import '../courseListing/CourseContainer.css';
 
 export default class MyCoursesContainer extends Component {
   render() {
+    const {
+      topic,
+      description,
+      descriptionProfile,
+      subject,
+      duration,
+      location,
+      tuition,
+      fee,
+      img
+    } = this.props.info;
+
+    const index = this.props.index;
     return (
-      <div>
-        <div className="col-sm-6 col-md-4 col-lg-3">
+      <div className="col-sm-6 col-md-4  ">
 
-                <div className="card-body" >
-                <h5 className="card-title">topic</h5>
-                <hr />
-                <strong className="card-text">Instructor: </strong>
-                <span className="card-text"> Mosttiee </span>
-                <hr />
-                <strong className="card-text limitP">Score: </strong>
-                <i className="star icon"></i>
-                <i className="half star icon"></i><br />
+        <div className="card shadow mb-5" data-toggle="modal" data-target={`#modal${index}`.toLowerCase()}>
+          <div className="modal" id={`modal${index}`.toLowerCase()}>
+            <div className="modal-dialog" >
+              <div className="modal-content" >
+                <div className="modal-header">
+                  <h5 className="modal-title pt-3 pb-3"> {topic} </h5>
+                  <button className="close">&times;</button>
                 </div>
 
-                <div className="card-footer d-flex">
-                <h5>subject</h5>
-                <h5 className="ml-auto">฿tuition</h5>
+                <div className="modal-body">
+                  <p className="modal-text"> {description} </p>
+                  <hr />
+                  <strong className="modal-text">Instructor: </strong>
+                  <a className="modal-text" href="#"> Mosttiee </a>
+                  <p className="modal-text"> {descriptionProfile} </p>
+                  <hr />
+                  <strong className="modal-text">location: </strong>
+                  <span className="modal-text"> {location}</span><br />
+                  <strong className="modal-text">Duration:</strong>
+                  <span className="modal-text"> {duration} </span><br />
+                  <strong className="modal-text">Fee: </strong>
+                  <span className="modal-text">฿{fee}</span>
                 </div>
+
+                <div className="modal-footer">
+                  <button className="btn btn-orange" >Edit</button>
+                  <button className="btn btn-secondary">Delete</button>
+                </div>
+              </div>
             </div>
+          </div>
+
+          <img className="card-img-top img-fluid" src={img} alt="unable to load file" />
+
+          <div className="card-body" >
+            <h5 className="card-title">{topic}</h5>
+            <hr />
+            <strong className="card-text">Instructor: </strong>
+            <span className="card-text"> Mosttiee </span>
+            <hr />
+            <strong className="card-text limitP">Score: </strong>
+            <i className="star icon"></i>
+            <i className="half star icon"></i><br />
+          </div>
+
+          <div className="card-footer d-flex">
+            <h5>{subject}</h5>
+            <h5 className="ml-auto">฿{tuition}</h5>
+          </div>
+
+        </div>
 
       </div>
     )
