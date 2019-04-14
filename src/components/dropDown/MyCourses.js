d3791334f985dafccccbb6f2ee26b0da8fdb31a3
5930486521-ST <sirawit_james@hotmail.co.th>
2019-04-14 23:37:19 +0700

    add redux and optimize code for mycourse

--- src/components/dropDown/MyCourses.js
+++ src/components/dropDown/MyCourses.js
@@ -10,76 +10,46 @@ import edu from "../../img/education.svg";
 import Loader from "../loader/Loader";
 import NoCourse from "./NoCourses";
 import TutorCourseContainer from "./TutorCourseContainer";
+import { Link } from "react-router-dom";
 
-export default class MyCourses extends Component {
+//query
+import axios from "axios";
+import querystring from "query-string";
+
+//redux
+import { EditCourseAction } from "../../actions/EditCourseAction";
+import { connect } from "react-redux";
+const mapStateToProps = state => ({});
+const mapDispatchToProps = dispatch => ({
+  setEditCourse: course => dispatch(EditCourseAction(course))
+});
+
+const tutor = "tutor"
+const learner = "learner"
+const notification = "notification"
+
+class MyCourses extends Component {
   constructor() {
     super();
     this.state = {
       coursesAsTutor: [],
-      coursesAsStudent: [],
+      coursesAsLearner: [],
       isLoading: false,
-      isTutor: true,
-      isLearner: null,
-      isNotification: null,
-      hasCourse: true
+      // isTutor: true,
+      // isLearner: null,
+      // isNotification: null,
+      // hasCourse: true
+      currentSubPage: tutor
     };
-    this.onTutorClick = this.onTutorClick.bind(this);
-    this.onLearnerClick = this.onLearnerClick.bind(this);
-    this.onNotification = this.onNotification.bind(this);
-  }
-
-  onTutorClick() {
-    this.setState({
-      isTutor: true,
-      isLearner: false,
-      isNotification: false
-    });
-    console.log(
-      "tutor:" +
-        this.state.isTutor +
-        " learner: " +
-        this.state.isLearner +
-        " noti:" +
-        this.state.isNotification
-    );
-  }
-
-  onLearnerClick() {
-    this.setState({
-      isTutor: false,
-      isLearner: true,
-      isNotification: false
-    });
-    console.log(
-      "tutor:" +
-        this.state.isTutor +
-        " learner: " +
-        this.state.isLearner +
-        " noti:" +
-        this.state.isNotification
-    );
-  }
-
-  onNotification() {
-    this.setState({
-      isTutor: false,
-      isLearner: false,
-      isNotification: true
-    });
-    console.log(
-      "tutor:" +
-        this.state.isTutor +
-        " learner: " +
-        this.state.isLearner +
-        " noti:" +
-        this.state.isNotification
-    );
+    // this.onTutorClick = this.onTutorClick.bind(this);
+    // this.onLearnerClick = this.onLearnerClick.bind(this);
+    // this.onNotification = this.onNotification.bind(this);
   }
 
   componentDidMount() {
     this.setState({ isLoading: true });
     this.fetchTutorCourses();
-    // this.fetchStudentCourses();
+    this.fetchLearnerCourses();
   }
 
   async fetchTutorCourses() {
@@ -95,15 +65,97 @@ export default class MyCourses extends Component {
     }
   }
 
-  async fetchStudentCourses() {
+  async fetchLearnerCourses() {
     try {
+      const response = await fetch(
+        // "get_courses_by_student"
+        "http://127.0.0.1:8000/api/get_courses?tutor=" +
+          window.localStorage.username
+      );
+      const courses = await response.json();
+      this.setState({ coursesAsLearner: courses.courses, isLoading: false });
     } catch (error) {
       console.log(error);
     }
   }
 
+  // onTutorClick() {
+  //   this.setState({
+  //     isTutor: true,
+  //     isLearner: false,
+  //     isNotification: false
+  //   });
+  //   console.log(
+  //     "tutor:" +
+  //       this.state.isTutor +
+  //       " learner: " +
+  //       this.state.isLearner +
+  //       " noti:" +
+  //       this.state.isNotification
+  //   );
+  // }
+
+  // onLearnerClick() {
+  //   this.setState({
+  //     isTutor: false,
+  //     isLearner: true,
+  //     isNotification: false
+  //   });
+  //   console.log(
+  //     "tutor:" +
+  //       this.state.isTutor +
+  //       " learner: " +
+  //       this.state.isLearner +
+  //       " noti:" +
+  //       this.state.isNotification
+  //   );
+  // }
+
+  // onNotification() {
+  //   this.setState({
+  //     isTutor: false,
+  //     isLearner: false,
+  //     isNotification: true
+  //   });
+  //   console.log(
+  //     "tutor:" +
+  //       this.state.isTutor +
+  //       " learner: " +
+  //       this.state.isLearner +
+  //       " noti:" +
+  //       this.state.isNotification
+  //   );
+  // }
+  onSubNavClick = event => {
+    this.setState({ currentSubPage : event.target.id });
+  };
+
+  onDeleteCourse = info => {
+    axios({
+      method: "POST",
+      url: "http://127.0.0.1:8000/api/delete_course",
+      crossDomain: true,
+      data: querystring.stringify({
+        token: window.localStorage.token,
+        id: info._id
+      }),
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      }
+    })
+      .then(() => (window.location = "/myCourses"))
+      .catch(error => {
+        alert("Failed to Delete the course\n" + error);
+        console.log(error);
+      });
+  };
+
+  onEditCourse = info => {
+    this.props.setEditCourse(info);
+  };
+
   render() {
-    const { coursesAsTutor, isLoading } = this.state;
+    const { coursesAsTutor, coursesAsLearner, isLoading,currentSubPage } = this.state;
     return (
       <div>
         <CowBg />
@@ -114,69 +166,99 @@ export default class MyCourses extends Component {
           </h1>
           <hr />
         </div>
+
         <div className="container mycoursePage">
           <div className="row text-center">
-            <div className="col border-right">
-              <a className="display-4 topic btn" onClick={this.onTutorClick}>
-                {this.state.isTutor ? (
+            <div className="col border-right" >
+              <a className="display-4 topic btn" >
+                {currentSubPage === tutor? (
                   <span className="underline-on-hover">As a tutor</span>
                 ) : (
-                  <span>As a tutor</span>
+                  <span id={tutor} onClick={this.onSubNavClick}>As a tutor</span>
                 )}
               </a>
               <img src={edu} style={{ width: "40px", height: "auto" }} />
             </div>
-            <div className="col border-right">
-              <a className="display-4 topic btn" onClick={this.onLearnerClick}>
+            <div className="col border-right" >
+              <a className="display-4 topic btn" >
                 {this.state.isLearner ? (
-                  <span className="underline-on-hover">As a learner</span>
+                  <span className="underline-on-hover" >As a learner</span>
                 ) : (
-                  <span>As a learner</span>
+                  <span id={learner} onClick={this.onSubNavClick}>As a learner</span>
                 )}
               </a>
               <img src={books} style={{ width: "40px", height: "auto" }} />
             </div>
-            <div className="col">
-              <a className="display-4 topic btn" onClick={this.onNotification}>
+            <div className="col" >
+              <a className="display-4 topic btn" >
                 {this.state.isNotification ? (
                   <span className="underline-on-hover">Notification</span>
                 ) : (
-                  <span>Notification</span>
+                  <span id={notification} onClick={this.onSubNavClick}>Notification</span>
                 )}
               </a>
               <img src={noti} style={{ width: "40px", height: "auto" }} />
             </div>
           </div>
           <hr />
+
           <div className="container">
             {this.state.isTutor ? (
               this.state.hasCourse ? (
-                <TutorCourseContainer isTutor={this.state.isTutor} />
+                coursesAsTutor.map(course => {
+                  return (
+                    <TutorCourseContainer info={course}>
+                      <div>
+                        <button
+                          className="btn btn-orange"
+                          onClick={() => this.onEditCourse(course)}
+                        >
+                          <Link to="/create_course" style={{ color: "white" }}>
+                            Edit
+                          </Link>
+                        </button>
+                        <span> </span>
+                        <button
+                          className="btn btn-secondary"
+                          onClick={() => this.onDeleteCourse(course)}
+                        >
+                          Delete
+                        </button>
+                      </div>
+                    </TutorCourseContainer>
+                  );
+                })
               ) : (
                 <NoCourse />
               )
             ) : null}
+
             {this.state.isLearner ? (
               this.state.hasCourse ? (
-                <TutorCourseContainer isTutor={this.state.isTutor} />
+                coursesAsLearner.map(course => (
+                  <TutorCourseContainer info={course}>
+                    <div>
+                      {" "}
+                      <button className="btn btn-success">Review</button>
+                    </div>
+                  </TutorCourseContainer>
+                ))
               ) : (
                 <NoCourse />
               )
             ) : null}
+
             {this.state.isNotification ? <div>noti</div> : null}
           </div>
         </div>
 
-        {/* old version */}
-        {/* <div className="row">
-          <div className="card-deck">
-            {coursesAsTutor.map((course, index) => (
-              <MyCoursesContainer key={index} info={course} index={index} />
-            ))}
-          </div>
-        </div> */}
         {isLoading ? Loader : null}
       </div>
     );
   }
 }
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MyCourses);
--- src/components/dropDown/TutorCourseContainer.js
+++ src/components/dropDown/TutorCourseContainer.js
@@ -1,80 +1,80 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import "../courseListing/CourseContainer.css";
 
 export default class TutorCourseContainer extends Component {
   constructor(props) {
     super(props);
   }
+
   render() {
+    const {
+      topic,
+      description,
+      descriptionProfile,
+      subject,
+      duration,
+      location,
+      tuition,
+      tutor_display,
+      fee,
+      img
+    } = this.props.info;
+
     return (
       <div className="courseCard">
         <div className="col-3">
-          <img src="https://media.licdn.com/dms/image/C510BAQFKXnLDglG5qA/company-logo_200_200/0?e=2159024400&v=beta&t=QKdDCJSmbaDE2mkA5ndgR407zzyfU4QEZQ0Hd769vBI" />
+          <img
+            className="card-img-top img-fluid"
+            src={img}
+            alt="unable to load picture"
+          />
         </div>
+
         <div className="col-5">
           <br />
           <br />
           <span className="text">
-            <strong>Topic : </strong> อุฟฟุฟวยหว่วยว้อย
-            เอนเยะทวยววยว้วยอุบวิมวุบวิม โอซ๊าส อิอิซ่า ห้าห้าบวก
+            <strong>Topic : </strong> {topic}
           </span>
           <br />
           <span className="text">
             <strong>Subject : </strong>
+            {subject}
           </span>
           <br />
           <span className="text">
             <strong>Instruction : </strong>
+            {tutor_display}
           </span>
           <br />
           <span className="text">
             <strong>Description : </strong>
+            {description}
           </span>
           <span className="text">
             <strong>Location : </strong>
+            {location}
           </span>
         </div>
+
         <div className="col-5">
           <br />
           <br />
           <span className="text">
             <strong>Duration : </strong>
+            {duration}
           </span>
           <br />
-          <strong>Tuition/Hour : </strong>
+          <strong>Tuition/Hour : </strong>฿{tuition}
           <br />
-          <strong>Joining fee : </strong>
+          <strong>Joining fee : </strong>฿{fee}
           <br />
-          <strong>Score : </strong>
+          <strong>Score : </strong>score??
           <br />
           <br />
-          {this.props.isTutor ? (
-            <div>
-              <button className="btn btn-orange" onClick={this.onEditCourse}>
-                <Link to="/create_course" style={{ color: "white" }}>
-                  Edit
-                </Link>
-              </button>
-              <span> </span>
-              <button
-                className="btn btn-secondary"
-                onClick={this.onDeleteCourse}
-              >
-                Delete
-              </button>
-            </div>
-          ) : (
-            <div>
-              {" "}
-              <button className="btn btn-success">Review</button>
-            </div>
-          )}
+          {this.props.children}
         </div>
       </div>
     );
   }
-}
-
-{
 }
