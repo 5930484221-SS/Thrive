0d797e9496883a5362c837da26cdb0d47ec03732
5930486521-ST <sirawit_james@hotmail.co.th>
2019-04-23 22:01:24 +0700

    merging from revert

--- src/components/dropDown/MyCourseContainer.js
+++ src/components/dropDown/MyCourseContainer.js
@@ -1,80 +1,154 @@
 import React, { Component } from "react";
-import "../courseListing/CourseContainer.css";
+import { Link } from "react-router-dom";
+import MyCourseContainerTemplate from "./MyCourseContainerTemplate";
+import MyCourseContentError from "./MyCourseContentError";
+import Loader from "../loader/Loader";
 
-export default class TutorCourseContainer extends Component {
-  constructor(props) {
-    super(props);
+//query
+import axios from "axios";
+import querystring from "query-string";
+
+//redux
+import { EditCourseAction } from "../../actions/EditCourseAction";
+import { connect } from "react-redux";
+const mapStateToProps = state => ({});
+const mapDispatchToProps = dispatch => ({
+  setEditCourse: course => dispatch(EditCourseAction(course))
+});
+
+class TeachingContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      coursesAsTutor: [],
+      isLoading: false
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      await this.setState({ isLoading: true });
+      const response = await fetch(
+        "http://127.0.0.1:8000/api/get_courses?tutor=" +
+          window.localStorage.username
+      );
+      const courses = await response.json();
+      this.setState({ coursesAsTutor: courses.courses, isLoading: false });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  onDeleteCourse = info => {
+    axios({
+      method: "POST",
+      url: "http://127.0.0.1:8000/api/delete_course",
+      crossDomain: true,
+      data: querystring.stringify({
+        token: window.localStorage.token,
+        id: info._id
+      }),
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      }
+    })
+      .then(() => (window.location = "/myCourses"))
+      .catch(error => {
+        alert("Failed to Delete the course\n" + error);
+        console.log(error);
+      });
+  };
+
+  onEditCourse = info => {
+    this.props.setEditCourse(info);
+  };
+
+  render() {
+    const { isLoading, coursesAsTutor } = this.state;
+    return (
+      <div>
+        {" "}
+        <MyCourseContentError>
+          {coursesAsTutor.map(course => (
+            <MyCourseContainerTemplate info={course}>
+              <div>
+                <button
+                  className="btn btn-orange"
+                  onClick={() => this.onEditCourse(course)}
+                >
+                  <Link to="/create_course" style={{ color: "white" }}>
+                    Edit
+                  </Link>
+                </button>
+                <span> </span>
+                <button
+                  className="btn btn-secondary"
+                  onClick={() => this.onDeleteCourse(course)}
+                >
+                  Delete
+                </button>
+              </div>
+            </MyCourseContainerTemplate>
+          ))}
+        </MyCourseContentError>
+        {isLoading ? Loader : null}
+      </div>
+    );
+  }
+}
+
+export const TeachingCourseContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TeachingContainer);
+
+export class LearningCourseContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      coursesAsLearner: [],
+      isLoading: false
+    };
+  }
+
+  async componentDidMount() {
+    this.setState({ isLoading: true });
+    try {
+      const response = await axios({
+        method: "POST",
+        url: "http://127.0.0.1:8000/api/get_courses_by_learner",
+        crossDomain: true,
+        data: querystring.stringify({
+          token: window.localStorage.token
+        }),
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        }
+      });
+      this.setState({
+        coursesAsLearner: response.data.courses,
+        isLoading: false
+      });
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   render() {
-    const {
-      topic,
-      description,
-      descriptionProfile,
-      subject,
-      duration,
-      location,
-      tuition,
-      tutor_display,
-      fee,
-      img
-    } = this.props.info;
-
+    const { isLoading, coursesAsLearner } = this.state;
     return (
-      <div className="courseCard container">
-        <div className="col-3">
-          <img
-            className="card-img-top img-fluid"
-            src={img}
-            alt="unable to load picture"
-            style={{ marginTop: "10%", marginBottom: "10%" }}
-          />
-        </div>
-
-        <div className="col-5">
-          <br />
-          <br />
-          <span className="text">
-            <strong>Topic : </strong> {topic}
-          </span>
-          <br />
-          <span className="text">
-            <strong>Subject : </strong>
-            {subject}
-          </span>
-          <br />
-          <span className="text">
-            <strong>Instruction : </strong>
-            {tutor_display}
-          </span>
-          <br />
-          <span className="text">
-            <strong>Description : </strong>
-            {description}
-          </span>
-          <span className="text">
-            <strong>Location : </strong>
-            {location}
-          </span>
-        </div>
-
-        <div className="col-5">
-          <br />
-          <br />
-          <span className="text">
-            <strong>Duration : </strong>
-            {duration}
-          </span>
-          <br />
-          <strong>Tuition/Hour : </strong>฿{tuition}
-          <br />
-          <strong>Joining fee : </strong>฿{fee}
-          <br />
-          <strong>Score : </strong>score??
-          <br />
-          <br />
-          {this.props.children}
-        </div>
+      <div>
+        <MyCourseContentError>
+          {coursesAsLearner.map(course => (
+            <MyCourseContainerTemplate info={course} key={course._id}>
+              <div>
+                {" "}
+                <button className="btn btn-success">Review</button>
+              </div>
+            </MyCourseContainerTemplate>
+          ))}
+        </MyCourseContentError>
+        {isLoading ? Loader : null}
       </div>
     );
   }
