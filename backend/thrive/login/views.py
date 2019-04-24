7d1fa9957747624110e19fced2b20c7be916587c
SSripilaipong <vappakarat@gmail.com>
2019-04-24 15:12:16 +0700

    bring user endpoint back for beem

--- backend/thrive/login/views.py
+++ backend/thrive/login/views.py
@@ -359,7 +359,7 @@ def get_tutors(request):
 
 @csrf_exempt
 @require_http_methods(["GET"])
-def user(request):
+def users(request):
     result_keys = {'user': 'username', 'first_name': 'firstName', 'last_name': 'lastName', 'nickname': 'nickname',
                    'display': 'displayName', 'address': 'address', 'phone_number': 'phoneNumber', 'email': 'email',
                    'contact': 'contact', 'is_admin': 'isAdmin'}
@@ -378,6 +378,25 @@ def user(request):
         return HttpResponseNotFound('The given username does not exist')
 
     response = JsonResponse({'users': users})
+    return set_response_header(response)
+
+
+@csrf_exempt
+@require_http_methods(["GET"])
+def user(request):
+    result_keys = {'user': 'username', 'first_name': 'firstName', 'last_name': 'lastName', 'nickname': 'nickname',
+                   'display': 'displayName', 'address': 'address', 'phone_number': 'phoneNumber', 'email': 'email',
+                   'contact': 'contact', 'is_admin': 'isAdmin'}
+
+    collection = mongo_db.get_collection('users')
+    username = request.GET.get('username')
+    match = collection.find_one({'user': username})
+
+    if not match:
+        return HttpResponseNotFound('The given username does not exist')
+
+    result = {v: match[k] for k, v in result_keys.items()}
+    response = JsonResponse(result)
     return set_response_header(response)
 
 
