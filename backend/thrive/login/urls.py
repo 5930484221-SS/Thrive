c045f1a6b470265ace9da59db26e190a455e0323
SSripilaipong <vappakarat@gmail.com>
2019-04-24 16:00:22 +0700

    add delete_user endpoint and adjust login and registration endpoints

--- backend/thrive/login/urls.py
+++ backend/thrive/login/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path('get_tutors', views.get_tutors, name='get_tutors'),
     path('user', views.user, name='user'),
     path('users', views.users, name='users'),
+    path('delete_user', views.delete_user, name='delete_user'),
     path('logout', views.logout, name='logout'),
     path('delete_course', views.delete_course, name='delete_course'),
     path('create_reserve', views.create_reserve, name='create_reserve'),
