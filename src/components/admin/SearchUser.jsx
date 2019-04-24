e84a64c35cdaab24595503061ad9237832a59a4e
Sarn Sornpaisarn <fsornpaisarn@gmail.com>
2019-04-24 21:33:29 +0700

    SearchUser almost done

--- src/components/admin/SearchUser.jsx
+++ src/components/admin/SearchUser.jsx
@@ -2,11 +2,9 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import Select from 'react-select';
 import querystring from 'query-string';
-
 import CowBg from '../CowBg';
 import UserContainer from './UserContainer';
 import SearchBar from '../courseListing/SearchBar';
-
 import Loader from '../loader/Loader';
 
 class SearchUser extends Component {
@@ -19,21 +17,26 @@ class SearchUser extends Component {
     };
   }
 
-  async componentDidMount() {
+  async refresh() {
     try {
       const response = await axios({
         method: 'GET',
         crossDomain: true,
-        url: 'http://localhost:8000/api/user?username', //require get_user (in db)
+        url: `http://localhost:8000/api/user?username=${this.state.search}`, //require get_user (in db)
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded'
           // "Access-Control-Allow-Origin": "*"
         }
       });
-      this.setState({ userList: response.data.user, isLoading: false });
+      console.log('from componentDidMount', response);
+      this.setState({ userList: response.data.users, isLoading: false });
     } catch (error) {
       console.log(error);
     }
+  }
+
+  componentDidMount() {
+    this.refresh();
   }
 
   onSearchChange(e) {
@@ -61,6 +64,7 @@ class SearchUser extends Component {
 
   render() {
     const { isLoading, search, userList } = this.state;
+    console.log({ userList });
     // take a look at the states in the console!
     console.log({ isLoading, search, userList });
 
@@ -78,17 +82,27 @@ class SearchUser extends Component {
           />
         </div>
         <div className="row">
-          {userList.length > 0 || isLoading ? (
-            userList.map((c, index) => (
-              <UserContainer key={index} info={c} index={index} />
-            ))
-          ) : (
-            <div className="display-4 m-auto">
-              No <span className="text-orange">Results</span>
-            </div>
-          )}
+          <div className="col-sm-2" />
+          <div className="col-sm-8">
+            <ul className="list-group">
+              {userList.length > 0 || isLoading ? (
+                userList.map((c, index) => (
+                  <li className="list-group-item" key={index}>
+                    <UserContainer
+                      info={c}
+                      index={index}
+                      refresh={this.refresh.bind(this)}
+                    />
+                  </li>
+                ))
+              ) : (
+                <div className="display-4 m-auto">
+                  No <span className="text-orange">Results</span>
+                </div>
+              )}
+            </ul>
+          </div>
         </div>
-        <UserContainer />
       </div>
     );
   }
