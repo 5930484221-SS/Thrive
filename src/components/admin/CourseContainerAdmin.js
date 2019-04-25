988b59755c385ea06b514453c572fada3ab4c31b
thamonwan tangchitdee <thamonwan_most@hotmail.com>
2019-04-25 19:50:04 +0700

    added courses and delete course function

---
+++ src/components/admin/CourseContainerAdmin.js
@@ -0,0 +1,185 @@
+import React, { Component } from "react";
+import "../courseListing/CourseContainer.css";
+import swal from "sweetalert";
+import loaderIcon from "../../img/loaderIcon.gif";
+
+import axios from "axios";
+import querystring from "query-string";
+
+class CourseContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      sendingRequest: false,
+      courseList: []
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      const response = await axios({
+        method: "GET",
+        crossDomain: true,
+        url: "http://localhost:8000/api/get_courses",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+          // "Access-Control-Allow-Origin": "*"
+        }
+      });
+      this.setState({ courseList: response.data.courses, isLoading: false });
+    } catch (error) {
+      console.log("fetch fails, please refresh the page");
+    }
+  }
+
+  onDelete = async () => {
+    try {
+      swal({
+        text: "Deleting this course...",
+        icon: loaderIcon,
+        buttons: false
+      });
+      await axios({
+        method: "POST",
+        url: "http://127.0.0.1:8000/api/delete_course", //delete_course(request) on database
+        crossDomain: true,
+        data: querystring.stringify({
+          token: window.localStorage.token
+        }),
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        }
+      });
+      await swal({
+        text: "Delete successfully",
+        icon: "success"
+      });
+    } catch (error) {
+      swal.stopLoading();
+      swal.close();
+      console.log(error.response);
+      if (error.response.status === 403) {
+        swal({
+          text: error.response.data,
+          icon: "error"
+        });
+      } else {
+        swal({
+          text: error.response.status + " " + error.response.statusText,
+          icon: "error"
+        });
+      }
+    }
+  };
+
+  render() {
+    const {
+      topic,
+      description,
+      descriptionProfile,
+      subject,
+      duration,
+      location,
+      tuition,
+      tutor_display,
+      fee,
+      img
+    } = this.props.info;
+    const { courseList } = this.state;
+
+    const index = this.props.index;
+
+    return (
+      <div className="col-sm-6 col-md-4 col-lg-3 px-4">
+        <div
+          className="card shadow mb-5"
+          data-toggle="modal"
+          data-target={`#modal${index}`.toLowerCase()}
+        >
+          <img
+            className="card-img-top img-fluid"
+            src={img}
+            alt="unable to load file"
+          />
+
+          <div className="card-body course-card-body">
+            <h5 className="card-title">{topic}</h5>
+            <hr />
+            <strong className="card-text">Instructor: </strong>
+            <span className="card-text"> {tutor_display} </span>
+            <hr />
+            <strong className="card-text limitP">Score: </strong>
+
+            <i className="star icon" />
+            <i className="half star icon" />
+            <br />
+          </div>
+
+          <div className="card-footer d-flex">
+            <h5>{subject}</h5>
+            <h5 className="ml-auto">฿{tuition}</h5>
+          </div>
+        </div>
+
+        {/* course container modal */}
+        <div
+          className="modal fade"
+          id={`modal${index}`.toLowerCase()}
+          tabIndex="-1"
+          role="dialog"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title pt-3 pb-3"> {topic} </h5>
+                <button className="close" data-dismiss="modal">
+                  &times;
+                </button>
+              </div>
+
+              <div className="modal-body">
+                <p className="modal-text"> {description} </p>
+                <hr />
+                <strong className="modal-text">Instructor: </strong>
+
+                <a className="modal-text" href="#">
+                  {tutor_display}
+                </a>
+                <p className="modal-text"> {descriptionProfile} </p>
+                <hr />
+                <strong className="modal-text">location: </strong>
+                <span className="modal-text"> {location}</span>
+                <br />
+                <strong className="modal-text">Duration:</strong>
+                <span className="modal-text"> {duration} </span>
+                <br />
+
+                <strong className="modal-text">Fee: </strong>
+                <span className="modal-text">฿{fee}</span>
+              </div>
+
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-orange"
+                  data-toggle="modal"
+                  data-target="#rating"
+                  data-dismiss="modal"
+                >
+                  See more review
+                </button>
+                <button className="btn btn-primary" onClick={this.onDelete}>
+                  Delete this course
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CourseContainer;
---
+++ src/components/admin/ListingAdmin.js
@@ -0,0 +1,318 @@
+import React, { Component } from "react";
+import axios from "axios";
+import Select from "react-select";
+import querystring from "query-string";
+
+import CowBg from "../CowBg";
+import CourseContainerAdmin from "./CourseContainerAdmin";
+import SearchBar from "../courseListing/SearchBar";
+
+import Loader from "../loader/Loader";
+
+import {
+  subjects,
+  locations,
+  tuitionFees,
+  joiningFees,
+  ratings
+} from "../courseListing/filterLists";
+
+class ListingAdmin extends Component {
+  constructor() {
+    super();
+    this.state = {
+      courseList: [],
+      isLoading: true,
+      search: "",
+      subject: [],
+      location: "",
+      tuitionMax: "",
+      feeMax: "",
+      rating: "",
+      isFilterOn: false
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      const response = await axios({
+        method: "GET",
+        crossDomain: true,
+        url: "http://localhost:8000/api/get_courses",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+          // "Access-Control-Allow-Origin": "*"
+        }
+      });
+      this.setState({ courseList: response.data.courses, isLoading: false });
+    } catch (error) {
+      console.log("fetch fails, please refresh the page");
+    }
+  }
+
+  onSearchChange(e) {
+    this.setState({
+      search: e.target.value
+    });
+  }
+
+  onChange(e) {
+    this.setState({
+      location: e.target.value
+    });
+  }
+
+  genQueryString(name, arr) {
+    let str = "";
+    for (let i = 0; i < arr.length; i++) {
+      str += `&${name}=${arr[i].value}`;
+    }
+    return str;
+  }
+
+  async onFilterSubmit(e) {
+    e.preventDefault();
+    const { subject, location, tuitionMax, feeMax, rating } = this.state;
+    this.setState({
+      courseList: [],
+      isLoading: true
+    });
+
+    let queryString = "";
+
+    if (subject) queryString += this.genQueryString("subject", subject);
+    if (location) queryString += `&location=${location}`;
+    if (tuitionMax) queryString += `&tuitionMax=${tuitionMax.value}`;
+    if (feeMax) queryString += `&feeMax=${feeMax.value}`;
+    if (rating) queryString += `&ratingMin=${rating.value}`;
+    queryString = queryString.slice(1);
+
+    console.log("queryString: ", queryString);
+    try {
+      const response = await axios({
+        method: "GET",
+        crossDomain: true,
+        url: `http://localhost:8000/api/get_courses?${queryString}`
+      });
+      this.setState({ courseList: response.data.courses, isLoading: false });
+    } catch (error) {
+      console.log("fail to search, please try again");
+    }
+  }
+
+  async onSearch(e) {
+    e.preventDefault();
+    const { search } = this.state;
+
+    this.setState({
+      courseList: [],
+      isLoading: true
+    });
+    try {
+      const response = await axios({
+        method: "GET",
+        url: `http://localhost:8000/api/get_courses?tutor=${search.trim()}`
+      });
+      console.log("courses fetched from search: ", response.data.courses);
+      this.setState(
+        { courseList: response.data.courses, isLoading: false },
+        () => console.log("courseList: ", this.state.courseList)
+      );
+    } catch (error) {
+      console.log("search fails, please try again");
+    }
+  }
+
+  handleChange(index, selectedOption) {
+    switch (index) {
+      case 0:
+        this.setState({
+          subject: selectedOption
+        });
+        break;
+      case 1:
+        this.setState({
+          location: selectedOption
+        });
+        break;
+      case 2:
+        this.setState({
+          tuitionMax: selectedOption
+        });
+        break;
+      case 3:
+        this.setState({
+          rating: selectedOption
+        });
+        break;
+      case 4:
+        this.setState({
+          feeMax: selectedOption
+        });
+        break;
+    }
+  }
+
+  render() {
+    const {
+      isLoading,
+      search,
+      subject,
+      location,
+      tuitionMax,
+      feeMax,
+      rating,
+      isFilterOn,
+      courseList
+    } = this.state;
+    // take a look at the states in the console!
+    console.log({ search, subject, location, tuitionMax, feeMax, rating });
+
+    return (
+      <div>
+        {isLoading ? Loader : null}
+        {/* <img src={section1} id="section1" /> */}
+        <CowBg />
+        <div className="py-4">
+          <SearchBar
+            placeHolder="Search Tutor"
+            onChange={this.onSearchChange.bind(this)}
+            onSearch={this.onSearch.bind(this)}
+            searchValue={search}
+          />
+          <div>
+            {isFilterOn && !isLoading ? (
+              <div className="card bg-transparent mt-3">
+                <div className="card-body">
+                  <div className="card-title text-center">
+                    <a
+                      onClick={() =>
+                        this.setState({
+                          isFilterOn: false,
+                          subject: null,
+                          location: null,
+                          tuitionMax: null,
+                          feeMax: null,
+                          rating: null
+                        })
+                      }
+                      href="#"
+                      className="h5 card-title text-center"
+                    >
+                      Filters <i className="fas fa-angle-up" />{" "}
+                    </a>
+                  </div>
+                  <form onSubmit={this.onFilterSubmit.bind(this)}>
+                    <div className="row my-3">
+                      <div className="col-lg-2">Subject</div>
+
+                      <div className="col-lg-10">
+                        <Select
+                          value={subject}
+                          onChange={this.handleChange.bind(this, 0)}
+                          options={subjects}
+                          isMulti={true}
+                          placeholder="Subject"
+                          name="subject"
+                        />
+                      </div>
+                    </div>
+                    <div className="row mb-3">
+                      <div className="col-lg-2">Location</div>
+
+                      <div className="col-lg-10">
+                        <input
+                          type="text"
+                          placeholder="Location"
+                          className="form-control"
+                          onChange={this.onChange.bind(this)}
+                          value={location}
+                        />
+                      </div>
+                    </div>
+                    <div className="row mb-3">
+                      <div className="col-lg-2">Tuition/Hour</div>
+
+                      <div className="col-lg-10">
+                        <Select
+                          value={tuitionMax}
+                          onChange={this.handleChange.bind(this, 2)}
+                          options={tuitionFees}
+                          placeholder="Tuition"
+                          name="tuitionMax"
+                        />
+                      </div>
+                    </div>
+                    <div className="row mb-3">
+                      <div className="col-lg-2">Joining Fee</div>
+
+                      <div className="col-lg-10">
+                        <Select
+                          value={feeMax}
+                          onChange={this.handleChange.bind(this, 4)}
+                          options={joiningFees}
+                          placeholder="Joining Fee"
+                          name="feeMax"
+                        />
+                      </div>
+                    </div>
+                    <div className="row mb-3">
+                      <div className="col-lg-2">Rating</div>
+
+                      <div className="col-lg-10">
+                        <Select
+                          value={rating}
+                          onChange={this.handleChange.bind(this, 3)}
+                          options={ratings}
+                          placeholder="Rating"
+                          name="rating"
+                        />
+                      </div>
+                    </div>
+                    <div classNfame="text-center">
+                      <input
+                        type="submit"
+                        value="Search"
+                        className="btn btn-outline-dark btn-lg"
+                      />
+                    </div>
+                  </form>
+                </div>
+              </div>
+            ) : (
+              <div className="card bg-transparent mt-3">
+                <div className="card-body text-center">
+                  <a
+                    href="#"
+                    className="card-title text-center h5"
+                    onClick={() =>
+                      this.setState({
+                        isFilterOn: true
+                      })
+                    }
+                  >
+                    Filters <i className="fas fa-angle-down" />
+                  </a>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="row">
+          {console.log(courseList)}
+          {courseList.length > 0 || isLoading ? (
+            courseList.map((c, index) => (
+              <CourseContainerAdmin key={index} info={c} index={index} />
+            ))
+          ) : (
+            <div className="display-4 m-auto">
+              No <span className="text-orange">Results</span>
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ListingAdmin;
